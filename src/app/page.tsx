b0e19280dfa40ab50f343c6940c68b0969144ae9
jonwensu <wens.sumande@gmail.com>
2024-12-27 10:42:50 +0800

    remove unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,51 +11,6 @@ import { useQuery } from "@tanstack/react-query";
 import { Copy, Trash } from "lucide-react";
 import { toast } from "sonner";
 
-const MEME_URL = "https://i.imgur.com/tDspe1M.jpeg";
-const OLD_MAN = "https://i.imgur.com/UukQHgi.jpeg";
-
-const NOT_COOL_BRO = "https://i.imgur.com/UonYUnV.jpeg";
-const DISTRACTED_BOYFRIEND = "https://i.imgur.com/Ms9LoaS.jpeg";
-const templates: MemeTemplate[] = [
-  {
-    id: "1",
-    url: MEME_URL,
-
-    description: "Drake hotline Bling",
-    texts: [
-      { content: "Hello World", x: 0.75, y: 0.25 },
-      { content: "Wazzup World", x: 0.75, y: 0.75 },
-    ],
-  },
-  {
-    id: "2",
-    url: OLD_MAN,
-    description: "Man Slamming Card on Table",
-    texts: [
-      { content: "Hello", x: 0.55, y: 0.07 },
-      { content: "World", x: 0.3, y: 0.8 },
-    ],
-  },
-  {
-    id: "3",
-    url: NOT_COOL_BRO,
-    description: "Not Cool Bro",
-    texts: [
-      { content: "Hello", x: 0.3, y: 0.3 },
-      { content: "World", x: 0.3, y: 0.8 },
-    ],
-  },
-  {
-    id: "4",
-    url: DISTRACTED_BOYFRIEND,
-    description: "Distracted Boyfriend",
-    texts: [
-      { content: "Hello", x: 0.63, y: 0.5 },
-      { content: "World", x: 0.3, y: 0.8 },
-    ],
-  },
-];
-
 const Home = () => {
   const { data } = useQuery({
     queryKey: ["memeTemplates"],
